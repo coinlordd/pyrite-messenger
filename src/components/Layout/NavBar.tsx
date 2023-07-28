4ad5ad18aa084de03cf890fcb31ffe72f3c1cbbb
coinlordd <coinlord.developer@gmail.com>
2023-07-28 18:38:04 +0200

    Update NavBar.tsx

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -1,8 +1,11 @@
-import styled from 'styled-components'
+import styled, { useTheme } from 'styled-components'
+import { GitHub } from 'react-feather'
 
 import { Web3Status } from '../Web3Status'
 import ThemeToggle from '~/theme/components/ThemeToggle'
 import Logo from './Logo'
+import { ButtonSecondary } from '../Common/Button'
+import { ExternalLink } from '../Link'
 
 const Nav = styled.nav`
   display: flex;
@@ -16,12 +19,36 @@ const Nav = styled.nav`
   z-index: 2;
 `
 
+const LogoWrapper = styled(ButtonSecondary)`
+  width: fit-content;
+  align-items: center;
+  height: 3rem;
+  border-radius: 10px;
+  background-color: ${({ theme }) => theme.background};
+  border: 1px solid ${({ theme }) => theme.backgroundOutline};
+  padding: 0.6rem;
+  border-radius: 10px;
+  color: ${({ theme }) => theme.white};
+  font-weight: 500;
+
+  :hover,
+  :focus {
+    outline: none;
+  }
+`
+
 export function Navbar() {
+  const theme = useTheme()
   return (
     <Nav>
       <Logo />
       <Web3Status />
       <ThemeToggle />
+      <ExternalLink href="https://github.com/coinlordd/pyrite-messenger">
+        <LogoWrapper>
+          <GitHub stroke={theme.textSecondary} />
+        </LogoWrapper>
+      </ExternalLink>
     </Nav>
   )
 }
